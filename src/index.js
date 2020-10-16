f5160f4f64f1ecf13979cb3651b88ca59a0499bf
nin3439 <ninok33_94@mail.ru>
2020-10-16 11:35:42 +0300

    feat: finish to implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+ itemArr = bracketsConfig.map(item => item.join(''));
+
+ for ( let i = 0; i < itemArr.length; i++) {
+   if (str.includes(itemArr[i])) {
+     str = str.replace(itemArr[i], '');
+     i = -1;
+   }
+ }
+ return str.length === 0 ? true : false;
 }
